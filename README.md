13b76fcc038d2aa22c5692b156112b6093f11a57
RumazQureshi <rumazqureshi@gmail.com>
2025-03-10 21:06:57 +0500

    updated

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Robot-Speaker
 created a robot speaker (text to speech) using pyttsx3 library which will take input text and speak that on your behalf
 <br>
+it will first welcome you and then ask for the text you want to speak
 also if someone ask who created you then it will reply that im created by Rumaz all other text will be speeched by the agent<br> 
 also if we write exit or quit then it will terminate the code 
--- robo.py
+++ robo.py
@@ -1,20 +1,32 @@
 # im importing pyttsx3 libray to convert text into speech
 import pyttsx3
-
 if __name__ == "__main__":# this line is added so that it can execute only in this program as a module
+    engine = pyttsx3.init()#this is initializing the text to speach engine
 
-    engine = pyttsx3.init() #this is initializing the text to speach engine
+    engine.setProperty("rate", 150)  #this will make the voice rate 140 which is normal human rate 120-150
+    voices = engine.getProperty("voices")#voice can be of male as well as of female for male id is 0 and for female id is 1
+    engine.setProperty("voice", voices[1].id)
 
     print("Welcome to Robot Speaker!\nCreated by Rumaz Qureshi")
+    engine.say("Welcome to Robot Speaker!\nCreated by Rumaz Qureshi")#welcome the user by reading this text
+    engine.runAndWait()
     # while loop so that it will gonna run continuously till we write exit or quit as input
     # also if user enter who created you it will give the answer that im created by Rumaz and again back to the loop
     while True:
         x = input("Enter what you want me to speak: ")
-        if x=="who created you":
-            print(engine.say("I am created by Rumaz"))
+        
+        if x.lower() in ["exit", "quit"]:
+            print("Exiting... Goodbye!")
+            engine.say("Goodbye!")
             engine.runAndWait()
-        elif x.lower() in ["exit", "quit"]:
             break
+        
+        elif x.lower() == "who created you":
+            response = "I am created by Rumaz Qureshi."
+            print(response)
+            engine.say(response)
+            engine.runAndWait()
+        
         else:
             engine.say(x)
             engine.runAndWait()
